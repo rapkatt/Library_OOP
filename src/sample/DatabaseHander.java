6749bbf4d54f91c5b95a9bd9a50406a2394b0970
rapkat <rapkat.baudunov@iaau.edu.kg>
2019-12-19 17:04:16 +0600

    connect DataBase

---
+++ src/sample/DatabaseHander.java
@@ -0,0 +1,21 @@
+package sample;
+
+import  java.sql.Connection;
+import  java.sql.DriverManager;
+import  java.sql.SQLException;
+
+public class DatabaseHander extends Configs{
+    Connection dbConnection;
+
+    public Connection getDbConnection()
+            throws ClassNotFoundException,SQLException{
+        String connectionString = "jdbc:mysql//"+ dbHost + ":"
+                + dbPort + "/" + dbName;
+        Class.forName("com.mysql.jdbc.Driver");
+
+        dbConnection = DriverManager.getConnection(connectionString,
+                dbUser,dbPass);
+
+        return dbConnection;
+    }
+}
